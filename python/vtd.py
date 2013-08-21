29a964a64230e26fca550e81a1ecba3dd782dfb1
Charles Hogg <charles.r.hogg@gmail.com>
2013-08-21 07:23:12 -0400

    Refresh system instead of clobbering it
    Otherwise, if we set the Contexts, they'll be gone before we can request the
    NextActions!

--- python/vtd.py
+++ python/vtd.py
@@ -4,5 +4,7 @@ import libvtd.trusted_system
 def UpdateTrustedSystem(file_name):
     """Make sure the TrustedSystem object is up to date."""
     global my_system
-    my_system = libvtd.trusted_system.TrustedSystem()
-    my_system.AddFile(file_name)
+    if 'my_system' not in globals():
+        my_system = libvtd.trusted_system.TrustedSystem()
+        my_system.AddFile(file_name)
+    my_system.Refresh()
