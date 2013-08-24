341943804032e77d55b5c40b3b2550bb2905bed3
Charles Hogg <charles.r.hogg@gmail.com>
2013-08-24 14:36:43 -0400

    Indicate due date status of actions

--- python/vtd.py
+++ python/vtd.py
@@ -1,3 +1,4 @@
+import datetime
 import libvtd.trusted_system
 
 
@@ -21,7 +22,8 @@ def NextActionDisplayText(next_action):
         by the NextAction text, followed by an indication of how the current
         time compares to the due date (if any).
     """
-    return PrependParentProjectText(next_action, next_action.text)
+    return PrependParentProjectText(next_action, next_action.text) \
+        + DueDateIndication(next_action)
 
 
 def PrependParentProjectText(node, text):
@@ -39,3 +41,72 @@ def PrependParentProjectText(node, text):
         return PrependParentProjectText(node.parent,
                                         node.parent.text + ' :: ' + text)
     return text
+
+
+def DueDateIndication(node):
+    """Indicates how node's due date compares to the current time.
+
+    Args:
+        node: A libvtd.node.Node object.
+
+    Returns:
+        Human-readable due date indication -- such as "Late 3 days", or
+        "Due in 50 minutes" -- wrapped in parenthesis and prepended by ' '.
+        If no due date, the empty string is returned.
+    """
+    if not node.due_date:
+        return ''
+    time_diff_secs = (datetime.datetime.now() - node.due_date).total_seconds()
+    fmt = ' ({})'.format('Due in {}' if time_diff_secs < 0 else 'Late {}')
+    return fmt.format(PrettyRelativeTime(time_diff_secs))
+
+
+def PrettyRelativeTime(time_diff_secs):
+    """Human-readable representation of a time difference.
+
+    E.g., '3 days', or '75 minutes'.
+
+    Args:
+        time_diff_secs: The time difference in seconds.  Note that the absolute
+        value is used.
+
+    Returns:
+        A human-readable representation of time_diff_secs.  Prefers smaller
+        numbers (hence, bigger time units), and goes to the next time unit when
+        we have at least 2 of them: so, we would have "119 minutes",
+        but "2 hours" instead of "120 minutes".
+    """
+    # Each tuple in the sequence gives the name of a unit, and the number of
+    # previous units which go into it.
+    weeks_per_month = 365.242 / 12 / 7
+    intervals = [('minute', 60), ('hour', 60), ('day', 24), ('week', 7),
+                 ('month', weeks_per_month), ('year', 12)]
+
+    unit, number = 'second', abs(time_diff_secs)
+    for new_unit, ratio in intervals:
+        new_number = float(number) / ratio
+        # If the new number is too small, don't go to the next unit.
+        if new_number < 2:
+            break
+        unit, number = new_unit, new_number
+    return Quantity(number, unit)
+
+
+def Quantity(number, singular, plural=None):
+    """A quantity, with the correctly-pluralized form of a word.
+
+    E.g., '3 hours', or '1 minute'.
+
+    Args:
+        number: The number used to decide whether to pluralize.  (Basically: we
+            will, unless it's 1.)
+        singular: The singular form of the term.
+        plural: The plural form of the term (default: singular + 's').
+
+    Returns:
+        A string starting with number, and ending with the appropriate form of
+        the term (i.e., singular if number is 1, plural otherwise).
+    """
+    plural = plural if plural else singular + 's'
+    number = int(number)
+    return '{} {}'.format(number, singular if number == 1 else plural)
